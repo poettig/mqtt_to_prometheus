1b4cebb831870d0dfe3f955f6080bcb6bb76ec79
Peter Oettig <peet1993@gmail.com>
2022-08-09 22:31:26 +0200

    Implemented outlier filtering, refactored code significantly.

--- converter.py
+++ converter.py
@@ -3,21 +3,14 @@
 import argparse
 import json
 import logging
+import math
 import re
 import signal
 import os
 
 import paho.mqtt.client as mqtt
+import typing
 from prometheus_client import Counter, Gauge, start_http_server
-
-relevant_message_types = [
-	"STATE",
-	"SENSOR",
-]
-
-label_keys = None
-messages_counter = None
-gauges = dict()
 
 
 # https://stackoverflow.com/a/1176023/6371499
@@ -35,111 +28,227 @@ class CamelToSnakeConverter:
 camel_to_snake_converter = CamelToSnakeConverter()
 
 
-def extract_metrics(data):
-	if isinstance(data, list):
-		# Extract metrics for each list entry
-		for entry in data:
-			yield from extract_metrics(entry)
-	elif isinstance(data, dict):
-		# Iterate whole dict
-		for key, value in data.items():
-			if isinstance(value, list) or isinstance(value, dict):
-				yield from extract_metrics(value)
-			else:
-				# Create gauge if value is numeric
-				if isinstance(value, int) or isinstance(value, float):
-					yield key, value
+class Metric:
+	def __init__(self, key, value, labels: dict):
+		self.key = key
+		self.value = value
+		self.labels = labels
+
+	def get_name(self):
+		labels_as_strings = [key + '=' + value for key, value in self.labels.items()]
+		return f"{self.key}{{{', '.join(labels_as_strings)}}}"
+
+	def __str__(self):
+		return f"{self.get_name()} {self.value}"
 
 
-def parse_metrics(data, labels):
-	for key, value in extract_metrics(data):
-		prefixed_key = f"tasmota_{camel_to_snake_converter.convert(key)}"
+class MetricsManager:
+	def __init__(self, config):
+		self.gauges: typing.Dict[str, Gauge] = dict()
+		self.expected_label_keys = None
+		self.message_counter = None
 
-		if prefixed_key not in gauges:
-			# Create new gauge
-			gauges[prefixed_key] = Gauge(prefixed_key, "Value of the " + prefixed_key + " reading", labels.keys())
-			logging.debug("Created gauge " + prefixed_key)
+		self.outlier_filters = {}
+		for entry in config["outlier_filters"]:
+			for metric_name in entry["metric_names"]:
+				self.outlier_filters[metric_name] = {
+					"rules": entry["rules"],
+					"max_dropped_values": entry["max_dropped_values"],
+					"already_dropped_values": 0
+				}
 
-		# Set new metric value
-		gauges[prefixed_key].labels(*labels.values()).set(value)
+		start_http_server(config["port"], config["bind_ip"])
 
-		if logging.root.level == logging.DEBUG:
-			labels_as_strings = [key + '=' + value for key, value in labels.items()]
-			logging.debug(f"Set value of {prefixed_key}{{{', '.join(labels_as_strings)}}} to {value}")
+	def extract_metrics(self, data):
+		if isinstance(data, list):
+			# Extract metrics for each list entry
+			for entry in data:
+				yield from self.extract_metrics(entry)
+		elif isinstance(data, dict):
+			# Iterate whole dict
+			for key, value in data.items():
+				if isinstance(value, list) or isinstance(value, dict):
+					yield from self.extract_metrics(value)
+				else:
+					# Create gauge if value is numeric
+					if isinstance(value, int) or isinstance(value, float):
+						yield key, value
 
-
-def on_connect(client, userdata, flags, rc):
-	if rc == 0:
-		logging.info("Connected to MQTT broker")
-		client.subscribe("tele/#")
-	else:
-		logging.error("Connected to MQTT broker with return code " + str(rc))
-		exithandler()
-
-
-def on_message(client, userdata, msg):
-	global label_keys
-	global messages_counter
-
-	topic = msg.topic
-	payload = msg.payload.decode()
-
-	logging.debug(f"Received message {topic} {payload}")
-
-	# Split and remove tele/
-	topic_elems = topic.split("/")[1:]
-
-	# Save name of metric
-	name = topic_elems[-1]
-	topic_elems = topic_elems[:-1]
-
-	# Extract labels
-	labels = {}
-	if len(topic_elems) % 2 != 0:
-		logging.error("Inner topic parts are not an even number of elements. Fix pls!")
-		logging.error(f"{topic_elems}")
-		exithandler()
-	else:
-		it = iter(topic_elems)
-		for key in it:
-			labels[key] = next(it)
-
-	# Check if the last part of the topic is one of the relevant message types
-	if name not in relevant_message_types:
-		logging.debug(f"Ignored message: {topic} {payload}")
-		return
-
-	# Check if the label keys match the stores ones. If none are stored, initialize it and the counter
-	if label_keys is None:
-		label_keys = list(labels.keys())
-		messages_counter = Counter(
-			"processed_messages",
-			"MQTT messages processed for this topic",
-			label_keys + ["type"]
-		)
-	elif label_keys != list(labels.keys()):
-		logging.error(
-			"Label keys [" + ",".join(labels.keys()) + "] do not match the keys stored from the first message."
+	def process_metrics(self, data: dict, labels):
+		# Check if the label keys match the stored ones. If none are stored, initialize it and the counter
+		if self.expected_label_keys is None:
+			self.expected_label_keys = list(labels.keys())
+			self.message_counter = Counter(
+				"tasmota_processed_messages",
+				"MQTT messages processed for this topic",
+				self.expected_label_keys
 			)
-		exithandler()
+		elif self.expected_label_keys != list(labels.keys()):
+			logging.error(
+				"Label keys [" + ",".join(labels.keys()) + "] do not match the keys stored from the first message."
+			)
+			exithandler()
 
-	labels_for_counter = list(labels.values()) + [name]
-	messages_counter.labels(*labels_for_counter).inc()
+		labels_for_counter = list(labels.values())
+		self.message_counter.labels(*labels_for_counter).inc()
 
-	# Convert every numeric value received into a gauge
-	try:
-		data = json.loads(payload)
-		parse_metrics(data, labels)
-	except ValueError:
-		logging.info("Ignored message: " + topic + " " + payload)
-		pass
+		for key, value in self.extract_metrics(data):
+			prefixed_key = f"tasmota_{camel_to_snake_converter.convert(key)}"
+			metric = Metric(prefixed_key, value, labels)
+
+			if self.is_outlier(metric):
+				continue
+
+			if prefixed_key not in self.gauges:
+				self.create_gauge(metric)
+
+			self.set_gauge_value(metric)
+
+			if logging.root.level == logging.DEBUG:
+				logging.debug(f"Set value of {metric.get_name()} to {metric.value}")
+
+	def create_gauge(self, metric: Metric):
+		self.gauges[metric.key] = Gauge(metric.key, "Value of the " + metric.key + " reading", metric.labels.keys())
+		logging.debug(f"Created gauge {metric.get_name()}")
+
+	def is_outlier(self, metric: Metric):
+		filter_info = self.outlier_filters.get(metric.key)
+		if filter_info is None:
+			# No filter defined, not an outlier
+			return False
+
+		gauge = self.gauges.get(metric.key)
+		if gauge is None:
+			# The gauge does not exist yet, no value to compare against
+			return False
+
+		for rule in filter_info["rules"]:
+			rule_type, rule_value = rule.split(":")
+			rule_value = float(rule_value)
+
+			x = gauge.labels(*metric.labels.values())
+			previous_value = x._value.get()
+
+			if rule_type == "diff":
+				if abs(metric.value - previous_value) > rule_value:
+					outlier_message = f"{metric.value} differs from previous value {previous_value} by more than {rule_value}."
+					break
+
+			elif rule_type == "above":
+				if metric.value > rule_value:
+					outlier_message = f"{metric.value} is above {rule_value}."
+					break
+
+			elif rule_type == "below":
+				if metric.value < rule_value:
+					outlier_message = f"{metric.value} is below {rule_value}."
+					break
+
+			elif rule_type == "value":
+				if math.isclose(metric.value, rule_value):
+					outlier_message = f"{metric.value} is a forbidden value."
+					break
+
+			else:
+				raise ValueError(f"Invalid rule type '{rule_type}'.")
+
+		else:
+			# No filter was hit
+			if filter_info["already_dropped_values"] != 0:
+				filter_info["already_dropped_values"] = 0
+				logging.info(f"No outlier filter hit for {metric}, reset drop counter.")
+			return False
+
+		filter_info["already_dropped_values"] += 1
+
+		if filter_info["already_dropped_values"] >= filter_info["max_dropped_values"]:
+			# Always accept when the maximum dropped values are reached
+			filter_info["already_dropped_values"] = 0
+			logging.warning(
+				f"Accepted detected outlier {metric} because maximum drops are reached."
+				f" Drop reason would have been '{outlier_message}'."
+			)
+			return False
+
+		logging.warning(
+			f"[{filter_info['already_dropped_values']}/{filter_info['max_dropped_values']}] Filtered outlier {metric}:"
+			f" {outlier_message}"
+		)
+		return True
+
+	def set_gauge_value(self, metric: Metric):
+		self.gauges[metric.key].labels(*metric.labels.values()).set(metric.value)
 
 
-def on_log(client, userdata, level, buf):
-	if level == mqtt.MQTT_LOG_ERR:
-		logging.error("MQTT ERROR: " + buf)
-	elif level == mqtt.MQTT_LOG_WARNING:
-		logging.warning("MQTT WARNING: " + buf)
+class MQTTManager:
+	relevant_message_types = [
+		"STATE",
+		"SENSOR",
+	]
+
+	def __init__(self, config: dict, metrics_manager: MetricsManager):
+		self.metrics_manager = metrics_manager
+
+		self.mqtt_client = mqtt.Client()
+		self.mqtt_client.on_connect = self.on_connect
+		self.mqtt_client.on_message = self.on_message
+		self.mqtt_client.on_log = self.on_log
+		if config["tls"]:
+			self.mqtt_client.tls_set_context()
+		self.mqtt_client.username_pw_set(config["user"], config["password"])
+		self.mqtt_client.connect(config["host"], config["port"], 60)
+		self.mqtt_client.loop_forever()
+
+	def on_connect(self, client, _, __, rc):
+		if rc == 0:
+			logging.info("Connected to MQTT broker")
+			client.subscribe("tele/#")
+		else:
+			logging.error("Connected to MQTT broker with return code " + str(rc))
+			exithandler()
+
+	def on_message(self, _, __, msg):
+		topic = msg.topic
+		payload = msg.payload.decode()
+
+		logging.debug(f"Received message {topic} {payload}")
+
+		# Split and remove tele/
+		topic_elems = topic.split("/")[1:]
+
+		# Save name of metric
+		name = topic_elems[-1]
+		topic_elems = topic_elems[:-1]
+
+		# Extract labels
+		labels = {}
+		if len(topic_elems) % 2 != 0:
+			logging.error("Inner topic parts are not an even number of elements. Fix pls!")
+			logging.error(f"{topic_elems}")
+			exithandler()
+		else:
+			it = iter(topic_elems)
+			for key in it:
+				labels[key] = next(it)
+
+		# Check if the last part of the topic is one of the relevant message types
+		if name not in MQTTManager.relevant_message_types:
+			logging.debug(f"Ignored message: {topic} {payload}")
+			return
+
+		# Convert every numeric value received into a gauge
+		try:
+			data = json.loads(payload)
+			self.metrics_manager.process_metrics(data, labels)
+		except ValueError as ex:
+			logging.info(f"Ignored message {topic} {payload}: {ex}")
+			pass
+
+	def on_log(self, _, level, buf):
+		if level == mqtt.MQTT_LOG_ERR:
+			logging.error("MQTT ERROR: " + buf)
+		elif level == mqtt.MQTT_LOG_WARNING:
+			logging.warning("MQTT WARNING: " + buf)
 
 
 def exithandler(signum=-1, stackframe=None):
@@ -161,6 +270,16 @@ if __name__ == "__main__":
 	parser.add_argument("--config", default="config.json", help="The configuration file for the converter.")
 	args = parser.parse_args()
 
+	# Setup logging
+	if args.debug:
+		log_level = logging.DEBUG
+	elif args.verbose:
+		log_level = logging.INFO
+	else:
+		log_level = logging.WARN
+
+	logging.basicConfig(level=log_level, format="[%(asctime)s] %(levelname)8s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
 	# Load config
 	if not os.path.isfile(args.config):
 		logging.critical(f"Config file '{args.config}' does not exist.")
@@ -168,7 +287,7 @@ if __name__ == "__main__":
 
 	with open(args.config, "r") as fh:
 		try:
-			config = json.load(fh)
+			conf = json.load(fh)
 		except json.JSONDecodeError as e:
 			logging.critical(f"Failed to decode config JSON: {e}")
 			exithandler()
@@ -177,25 +296,5 @@ if __name__ == "__main__":
 	signal.signal(signal.SIGINT, exithandler)
 	signal.signal(signal.SIGTERM, exithandler)
 
-	# Setup logging
-	if args.debug:
-		logging.basicConfig(level=logging.DEBUG)
-	elif args.verbose:
-		logging.basicConfig(level=logging.INFO)
-	else:
-		logging.basicConfig(level=logging.WARN)
-
 	# Start prometheus server
-	prometheus_exporter_config = config["prometheus_exporter"]
-	start_http_server(prometheus_exporter_config["port"], prometheus_exporter_config["bind_ip"])
-
-	mqtt_config = config["mqtt"]
-	mqtt_client = mqtt.Client()
-	mqtt_client.on_connect = on_connect
-	mqtt_client.on_message = on_message
-	mqtt_client.on_log = on_log
-	if mqtt_config["tls"]:
-		mqtt_client.tls_set_context()
-	mqtt_client.username_pw_set(mqtt_config["user"], mqtt_config["password"])
-	mqtt_client.connect(mqtt_config["host"], mqtt_config["port"], 60)
-	mqtt_client.loop_forever()
+	MQTTManager(conf["mqtt"], MetricsManager(conf["prometheus_exporter"]))
