c67ce97919f15c76cdb25b7b2da9968967568041
Peter Oettig <peet1993@gmail.com>
2023-10-26 02:28:21 +0200

    Add config shorthand

--- converter.py
+++ converter.py
@@ -371,7 +371,7 @@ def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-d", "--debug", action="store_true", help="Enable debug output.")
 	parser.add_argument("-v", "--verbose", action="store_true", help="Enable verbose output.")
-	parser.add_argument("--config", default="config.json", help="The configuration file for the converter.")
+	parser.add_argument("-c", "--config", default="config.json", help="The configuration file for the converter.")
 	args = parser.parse_args()
 
 	# Setup logging
